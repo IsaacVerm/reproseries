a992ec7cb1c16c400cb6fe60c009aaa0883109e1
IsaacVerm <isaacverm@gmail.com>
2019-11-12 15:47:56 +0100

    document title and description plotAvgTemperaturesByYear

--- R/visualize.R
+++ R/visualize.R
@@ -1,7 +1,32 @@
-plotAvgTemperaturesByYear <- function(avg_temperatures) {
+#' Plot of average temperatures by year
+#'
+#' \code{plotAvgTemperaturesByYear} plots the average temperature by year in a line graph
+plotAvgTemperaturesByYear <- function(avg_temperatures,
+                                      place = "Nottingham") {
+
+  createTitle <- function(avg_temperatures, place) {
+    firstYear <- function(avg_temperatures) {
+      min(avg_temperatures$year)
+    }
+
+    lastYear <- function(avg_temperatures) {
+      max(avg_temperatures$year)
+    }
+
+    paste0(
+      "average temperature by year in ",
+      place,
+      " (",
+      firstYear(avg_temperatures),
+      "-",
+      lastYear(avg_temperatures),
+      ")"
+    )
+  }
+
   ggplot2::ggplot(data = avg_temperatures,
                   ggplot2::aes(x = year, y = avgTemperature)) +
     ggplot2::geom_line(group = 1) +
-    ggplot2::labs(title = "average temperature by year in Nottingham (1920-1939)",
+    ggplot2::labs(title = createTitle(avg_temperatures, place),
                   y = "average temperature")
 }
