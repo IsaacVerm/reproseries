d70c652a4028f8ea9db745a1c1bbfde69328740a
IsaacVerm <isaacverm@gmail.com>
2019-11-12 16:43:31 +0100

    document plotAvgTemperaturesByYear

--- R/visualize.R
+++ R/visualize.R
@@ -1,6 +1,20 @@
 #' Plot of average temperatures by year
 #'
-#' \code{plotAvgTemperaturesByYear} plots the average temperature by year in a line graph
+#' \code{plotAvgTemperaturesByYear} plots the average temperature by year in a line graph.
+#'
+#' This function plots the average temperature by year.
+#' It also automatically updates the title so there's no need to specify the title yourself.
+#'
+#' @param avg_temperatures Data.frame. Has years and their corresponding average temperature.
+#' @param place String. It's assumed this function is called with the nottem built-in dataset.
+#' This dataset records the temperatures in Nottingham. Nottingham is the default argument.
+#' You can override this argument if the need arises.
+#' @return ggplot object
+#' @examples
+#' avg_temperatures <- data.frame(year = c(1920, 1921, 1922, 1923),
+#'                                avgTemperature = c(5, 10, 8, 9))
+#' plotAvgTemperaturesByYear(avg_temperatures)
+#' @export
 plotAvgTemperaturesByYear <- function(avg_temperatures,
                                       place = "Nottingham") {
 
