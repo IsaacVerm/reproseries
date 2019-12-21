c094f6d4c23612d8c88c88f92d9b6ffcd3d30786
IsaacVerm <isaacverm@gmail.com>
2019-12-21 19:52:50 +0100

    add analysis DAG
    Simplest dag possible to run the script to tidy the data.
    
    Some issues still:
    - the bash command is an absolute path and should be relative
    - not all default arguments are necessary (the analysis DAG is based on the Airflow example DAG)

---
+++ airflow/dags/analysis.py
@@ -0,0 +1,31 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'analysis', default_args=default_args, schedule_interval=timedelta(days=1))
+
+t1 = BashOperator(
+    task_id='tidy',
+    bash_command='/Users/isaacverminck/Documents/reproseries/exec/tidy-script.R ',
+    dag=dag)
