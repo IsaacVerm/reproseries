d0003fac2d2030b4da897cdf5a2d8f2badc22b91
IsaacVerm <isaacverm@gmail.com>
2019-12-22 16:35:17 +0100

    update temperatures DAG with clean and transform tasks

--- airflow/dags/temperatures.py
+++ airflow/dags/temperatures.py
@@ -6,7 +6,20 @@ import os
 dag = DAG(dag_id='temperatures',
           start_date=datetime(2019, 1, 1))
 
-t1 = BashOperator(
+clean = BashOperator(
+    task_id='clean',
+    bash_command=f'rm -f {os.getcwd()}/data/*',
+    dag=dag
+)
+
+tidy = BashOperator(
     task_id='tidy',
-    bash_command=f'{os.getcwd()}/exec/tidy-script.R -p {os.getcwd()}/data/tidy-temperatures.csv',
+    bash_command=f'{os.getcwd()}/exec/tidy-script.R {os.getcwd()}/data/tidy-temperatures.csv',
     dag=dag)
+
+transform = BashOperator(
+    task_id='transform',
+    bash_command=f'{os.getcwd()}/exec/transform-script.R {os.getcwd()}/data/tidy-temperatures.csv {os.getcwd()}/data/transform-temperatures.csv',
+    dag=dag)
+
+clean >> tidy >> transform
